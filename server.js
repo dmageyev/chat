a385fa043701de4c5d2a7e95c69a085f7a084a4a
dmageyev <dmytro.aheiev@nure.ua>
2026-05-06 15:08:11 +0300

    Leson2_fin

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-const http =  require ('http');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const pathToIndex = path.join(__dirname, 'static', 'index.html');
-const indexHtmlFile = fs.readFileSync(pathToIndex);
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
+const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
 
-
-const server = http.createServer((req,res) =>{
-    if(req.url === '/'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(indexHtmlFile);
-    } else {
-        res.writeHead(404);
-        res.end('Not Found');
+const server = http.createServer((req, res) => {
+    switch(req.url) {
+        case '/': return res.end(indexHtmlFile);
+        case '/script.js': return res.end(scriptFile);
+        case '/style.css': return res.end(styleFile);
     }
-
+    return res.end('Error 404');
 });
 
+
 server.listen(3000);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected. id - ' + socket.id);
+  let userNickname = 'user';
+
+  socket.on('set_nickname', (nickname) => {
+    userNickname = nickname;
+  });
+
+  socket.on('new_message', (message) => {
+    io.emit('message', userNickname + ' : ' + message);
+  });
+});
--- static/script.js
+++ static/script.js
@@ -1,1 +1,21 @@
-alert('Welcome to Site');
+const socket = io();
+const messages = document.getElementById('messages');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+
+
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+    if (input.value) {
+        socket.emit('new_message', input.value);
+        input.value = '';
+    }
+});
+
+socket.on('message', function(msg){
+    var item = document.createElement('li');
+    item.textContent = msg;
+    console.log(msg);
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+});
